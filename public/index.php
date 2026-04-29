9cbf3d25402b81574ec5e1d1168534713f6ac2de
lolgamebot <rageandfury2@gmail.com>
2026-04-29 20:53:15 +0800

    removed unneccessary comments

--- public/index.php
+++ public/index.php
@@ -1,21 +1,5 @@
 <?php
 
-// require '../helpers.php';
-
-// require basePath('Router.php');
-
-// require basePath('Database.php');
-
-// $router = new Router();
-
-// $routes = require basePath('routes.php');
-
-// $uri = $_SERVER['REQUEST_URI'];
-
-// $method = $_SERVER['REQUEST_METHOD'];
-
-// $router->route($uri,$method);
-
 require __DIR__ . '/../helpers.php';
 
 require basePath('Router.php');
